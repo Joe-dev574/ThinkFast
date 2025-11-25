285dfa065b66bb2840ba0973edfb4602ef4a7204
Rangerboy574 <jdeweese574@icloud.com>
2025-11-25 11:21:53 -0600

    Initial Commit

---
+++ ThinkFast/ThinkFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkFastApp.swift
+//  ThinkFast
+//
+//  Created by Joseph DeWeese on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ThinkFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThinkFastTests/ThinkFastTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkFastTests.swift
+//  ThinkFastTests
+//
+//  Created by Joseph DeWeese on 11/25/25.
+//
+
+import Testing
+@testable import ThinkFast
+
+struct ThinkFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
